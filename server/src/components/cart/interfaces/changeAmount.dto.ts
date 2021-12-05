726615c549755834711de95dfebcdfdd6f0cdb67
mxxnseat <kirill_shkarun@mail.ru>
2021-12-05 21:34:52 +0200

    fully validation in controllers. Iiko download images

--- server/src/components/cart/interfaces/changeAmount.dto.ts
+++ server/src/components/cart/interfaces/changeAmount.dto.ts
@@ -1,4 +1,10 @@
+import { IsMongoIdObject } from "src/common/decorators/mongoIdValidate.decorator";
+import { IsNumber } from "class-validator";
+
 export class ChangeAmountDTO {
+    @IsNumber()
     public amount: number;
+
+    @IsMongoIdObject()
     public cartId: UniqueId;
 }
--- server/src/filters/validation.filter.ts
+++ server/src/filters/validation.filter.ts
@@ -1,19 +1,19 @@
 import {
     ArgumentsHost,
+    BadRequestException,
     Catch,
     ExceptionFilter,
     HttpException
 } from "@nestjs/common";
 import { Request, Response } from "express";
 
-@Catch()
+@Catch(BadRequestException)
 export class ValidationException implements ExceptionFilter {
     catch(exception: HttpException, host: ArgumentsHost) {
         const context = host.switchToHttp();
         const request = context.getRequest<Request>();
         const response = context.getResponse<Response>();
         const status = exception.getStatus();
-
         const excres = exception.getResponse();
         const message =
             typeof excres === "object"
