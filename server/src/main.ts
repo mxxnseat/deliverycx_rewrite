26de7909e3cb775ee4718f977feca5312a7f8c10
mxxnseat <kirill_shkarun@mail.ru>
2021-12-13 02:51:59 +0200

    конфиг докера

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,10 +5,7 @@ import { doc } from "./docs/api.doc";
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
-        origin:
-            process.env.NODE_ENV === "development"
-                ? "http://localhost:3000"
-                : "/",
+        origin: process.env.CLIENT_PATH,
         credentials: true
     });
 
