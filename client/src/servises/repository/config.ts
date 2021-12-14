3f9a52f5bd49bafcbe446c0c6bcf1f0da1d6d73a
mxxnseat <kirill_shkarun@mail.ru>
2021-12-14 20:27:29 +0200

    ендпоинт /api

--- client/src/servises/repository/config.ts
+++ client/src/servises/repository/config.ts
@@ -1,7 +1,10 @@
 export const config = {
-    REACT_APP_API_URL: process.env.NODE_ENV === "development" ? "http://localhost:5000" : "https://45.90.32.114/"
-}
+    REACT_APP_API_URL:
+        process.env.NODE_ENV === "development"
+            ? "http://localhost:5000"
+            : "/api"
+};
 //http://localhost:6661
 export const mock = {
-    REACT_APP_API_URL:"http://localhost:8765"
-}
+    REACT_APP_API_URL: "http://localhost:8765"
+};
