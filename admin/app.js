b8bc0573814b11573554d23e25eaa3406820b754
mxxnseat <kirill_shkarun@mail.ru>
2021-12-28 23:32:15 +0200

    self update

--- admin/app.js
+++ admin/app.js
@@ -1,28 +1,26 @@
-import express from "express";
-import {exec} from "child_process";
-
+const path = require("path");
+const {exec} = require("child_process");
+const express = require("express");
 const app = express();
 
 
+app.get("/", (req, res)=>{
+        console.log("we are updating")
 
-app.post("/update", (req,res)=>{
-    console.log("we are updating")
-
-    exec("cd .. && docker-compose -f ./docker-compose.iiko.yml up",(error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-});
+    exec("cd .. && docker-compose -f ./docker-compose.iiko.yml up", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+            console.log(`stdout: ${stdout}`);
+    });
+    
     res.status(200);
 });
-
-
-app.listen(6000, ()=>{
+app.listen(7000, ()=>{
     console.log("start")
 })
