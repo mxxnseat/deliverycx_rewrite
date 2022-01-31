1867d35d062bd3ec95c24fac93fe9119a89bae5f
xirurgx3d <xirurgx3d@gmail.com>
2022-01-31 17:03:21 +0300

    скрытие карты, на самовывоз
    переходы через роутер

--- client/src/application/components/core/Cart/CartChoice.tsx
+++ client/src/application/components/core/Cart/CartChoice.tsx
@@ -5,9 +5,12 @@ import { fetchAllCart, setOrderType } from "servises/redux/slice/cartSlice";
 import { useSelector } from 'react-redux';
 import { RootState } from 'servises/redux/createStore';
 import { CART_CHOICE } from "application/contstans/cart.const";
+import { useHistory } from 'react-router-dom';
+import { ROUTE_APP } from "application/contstans/route.const";
 
 const CartChoise: FC = () => { 
     const dispatch = useDispatch()
+    const history = useHistory()
     const activeChoice = useSelector((state:RootState) => state.cart.orderType)
 
     const deliveryCN = cn("cart__choice__item", { active: activeChoice === CART_CHOICE.COURIER });
@@ -22,8 +25,14 @@ const CartChoise: FC = () => {
 
     return (
         <div className="cart__choice">
-            <div className={deliveryCN} onClick={() => handlerChoice(CART_CHOICE.COURIER)}>Доставка</div>
-            <div className={pickupCN} onClick={() => handlerChoice(CART_CHOICE.PICKUP)}>Самовывоз</div>
+        <div className={deliveryCN} onClick={() => {
+          handlerChoice(CART_CHOICE.COURIER)
+          history.push(ROUTE_APP.CART.CART_DELIVERY)
+        }}>Доставка</div>
+        <div className={pickupCN} onClick={() => {
+          handlerChoice(CART_CHOICE.PICKUP)
+          history.push(ROUTE_APP.CART.CART_PICKUP)
+        }}>Самовывоз</div>
             <div className={onspotCN} >На месте</div>
         </div>
     )
