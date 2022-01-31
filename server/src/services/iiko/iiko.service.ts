7cff5fc6dcbe9ed8344124e0579ae0ace0f51a91
mxxnseat <kirill_shkarun@mail.ru>
2022-01-31 15:54:50 +0200

    sum in iiko price

--- server/src/services/iiko/iiko.service.ts
+++ server/src/services/iiko/iiko.service.ts
@@ -79,7 +79,7 @@ export class IikoService implements IIiko {
                       id: deliveryProduct.id,
                       name: deliveryProduct.name,
                       amount: 1,
-                      price: deliveryPrice
+                      sum: deliveryPrice
                   }
                 : undefined
         ].filter(Boolean);
