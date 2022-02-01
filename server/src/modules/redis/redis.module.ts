e10a07d539ea07f4af4e0a889908e47bc9bab319
mxxnseat <kirill_shkarun@mail.ru>
2022-02-01 11:32:34 +0200

    gitignore modules/redis

---
+++ server/src/modules/redis/redis.module.ts
@@ -0,0 +1,17 @@
+import { Module } from "@nestjs/common";
+import * as Redis from "redis";
+import { REDIS } from "./redis.constants";
+
+@Module({
+    providers: [
+        {
+            provide: REDIS,
+            useValue: Redis.createClient({
+                port: +process.env.REDIS_PORT,
+                host: process.env.REDIS_HOST
+            })
+        }
+    ],
+    exports: [REDIS]
+})
+export class RedisModule {}
