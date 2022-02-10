a356ef405b12dcb8d32685f6957345f928527d31
mxxnseat <kirill_shkarun@mail.ru>
2022-02-10 16:36:42 +0200

    interceptor bot

--- server/src/services/duplicateBot/bot.axios.ts
+++ server/src/services/duplicateBot/bot.axios.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import axios, { AxiosInstance } from "axios";
 import { Bot } from "./interfaces";
+import { ServiceUnavailableException } from "@nestjs/common";
 
 @Injectable()
 export class BotAxios {
@@ -14,6 +15,13 @@ export class BotAxios {
         this.axios = axios.create({
             baseURL: process.env.BOT_URL
         });
+
+        this.axios.interceptors.response.use(
+            (response) => response,
+            (error) => {
+                throw new ServiceUnavailableException();
+            }
+        );
     }
 
     async sendDuplicate(
