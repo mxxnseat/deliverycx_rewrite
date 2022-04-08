ef323d0aeb189ceec7966c770db2f7bf16494282
xirurgx3d <xirurgx3d@gmail.com>
2022-04-08 17:02:07 +0300

    iiko

--- iiko/src/pulling.ts
+++ iiko/src/pulling.ts
@@ -102,7 +102,7 @@ class IikoRequester {
                 const objectId = await OrganizationModel.findOneAndUpdate(
                     { id: guid },
                     {
-                        $setOnInsert: {
+                        $set: {
                             id: guid,
                             city: cityId,
                             address: {
