df2c8bd813fde639459a5e1520237a9218675baf
xirurgx3d <xirurgx3d@gmail.com>
2022-04-08 17:26:00 +0300

    Update useCase.Points.ts

--- client/src/domain/use-case/useCaseLocation/useCase.Points.ts
+++ client/src/domain/use-case/useCaseLocation/useCase.Points.ts
@@ -97,7 +97,7 @@ export function usePoints() {
           
             // передаем случайный индекс
             const randomindex = Math.floor(Math.random() * data.length)
-
+            console.log(index,statePoint)
             dispatchPoint({
                 type: ReducerActionTypePoints.slidePoint,
                 payload: index
